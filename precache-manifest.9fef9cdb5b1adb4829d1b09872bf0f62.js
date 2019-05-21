8a5c0647f9cdf3fb14102095817a6124933590d0
Daniel Tian <danieltian31@yahoo.com>
2019-05-21 09:09:25 -0500

    Updates

---
+++ precache-manifest.9fef9cdb5b1adb4829d1b09872bf0f62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ab080e6c5262f35a1312fa6bbf7ec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85bdfbff4cd4658ea9ee",
+    "url": "/static/css/main.cceae99b.chunk.css"
+  },
+  {
+    "revision": "5950b32a7990936d93c9",
+    "url": "/static/js/2.8ebcb166.chunk.js"
+  },
+  {
+    "revision": "85bdfbff4cd4658ea9ee",
+    "url": "/static/js/main.adfc83ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0df055b9329f9166ce29ca2f2b53216e",
+    "url": "/static/media/LiveMerch.0df055b9.png"
+  },
+  {
+    "revision": "7da6d8ea535c28c0e64016bd9972db8e",
+    "url": "/static/media/TweetTrace.7da6d8ea.png"
+  },
+  {
+    "revision": "ab2745eedcaa38ed3f05efc039022dd9",
+    "url": "/static/media/background.ab2745ee.jpeg"
+  },
+  {
+    "revision": "bbbaaf89229d8a3ff778f287e8303aa4",
+    "url": "/static/media/missenplace.bbbaaf89.png"
+  }
+]);
